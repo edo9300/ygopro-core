c1601e6ac453fd05ace7efd0a245602d72887965
edo9300 <edoardo762@gmail.com>
2018-02-09 17:57:46 +0100

    added lua linking

--- premake4.lua
+++ premake4.lua
@@ -2,6 +2,7 @@ project "ocgcore"
 	kind "StaticLib"
 
 	files { "**.cc", "**.cpp", "**.c", "**.hh", "**.hpp", "**.h" }
+    links { "lua" }
 	buildoptions { "-Wall", "-Wextra", "-pedantic" }
 	
 	configuration "windows"
