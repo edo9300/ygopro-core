90ac7dec31f1d27e581b4a4f47611ca8e79af435
edo9300 <edoardo762@gmail.com>
2018-11-24 16:43:54 +0100

    Remove useless lua folders inclusion

--- premake4.lua
+++ premake4.lua
@@ -7,8 +7,7 @@ project "ocgcore"
 		links { "lua" }
 		includedirs { "../lua" }
 	configuration "not vs*"
-        buildoptions { "-std=c++14" }
+		buildoptions { "-std=c++14" }
 	configuration "not windows"
 		links { "lua5.3-c++" }
 		buildoptions { "-std=c++14", "-Wno-unused-parameter", "-pedantic" }
-		includedirs { "/usr/include/lua", "/usr/include/lua5.3", "/usr/include/lua/5.3" }
