2a4386f3ed788f6e7e43b197c404bbf13b558b9a
edo9300 <edoardo762@gmail.com>
2018-11-24 16:53:37 +0100

    Will this premake madness ever end

--- premake4.lua
+++ premake4.lua
@@ -5,9 +5,10 @@ project "ocgcore"
 	
 	configuration "windows"
 		links { "lua" }
-		includedirs { "lua" }
+		includedirs { "../lua" }
 	configuration "not vs*"
-		buildoptions { "-std=c++14" }
+        buildoptions { "-std=c++14" }
 	configuration "not windows"
-		links { "lua5.3-c++" }
+		links { "lua5.3++" }
 		buildoptions { "-std=c++14", "-Wno-unused-parameter", "-pedantic" }
+		includedirs { "/usr/include/lua5.3" }
