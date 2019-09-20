8bf18d305fdcaafeb53f2a9aada884ff46bb71da
kevinlul <6320810+kevinlul@users.noreply.github.com>
2019-09-20 08:20:40 -0400

    ocgcore.lib no longer generated

--- travis/predeploy.sh
+++ travis/predeploy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 mkdir -p $DEPLOY_DIR
 cp LICENSE README.md $DEPLOY_DIR
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-	mv build/$BUILD_CONFIG/ocgcore.dll build/$BUILD_CONFIG/ocgcore.lib build/$BUILD_CONFIG/ocgcore_static.lib $DEPLOY_DIR
+	mv build/$BUILD_CONFIG/ocgcore.dll build/$BUILD_CONFIG/ocgcore_static.lib $DEPLOY_DIR
 	powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Compress-Archive -Path $DEPLOY_DIR -DestinationPath ocgcore-$TRAVIS_OS_NAME"
 else
 	mv build/libocgcore_static.a $DEPLOY_DIR
