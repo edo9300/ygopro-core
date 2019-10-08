8b94fc121fd76da4ca0dc7c5acf87afb6041b464
kevinlul <6320810+kevinlul@users.noreply.github.com>
2019-10-08 16:35:04 -0400

    [Travis][Windows] Stop building vcpkg and just use cache

--- travis/install-lua.sh
+++ travis/install-lua.sh
@@ -6,13 +6,11 @@ TRAVIS_OS_NAME=${1:-$TRAVIS_OS_NAME}
 CXX=${CXX:-g++}
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-	git clone --depth=1 https://github.com/Microsoft/vcpkg.git "$VCPKG_ROOT"
+	mkdir -p "$VCPKG_ROOT"
 	cd "$VCPKG_ROOT"
 	curl --retry 5 --connect-timeout 30 --location --remote-header-name --output installed.zip "$VCPKG_CACHE_ZIP_URL"
-	unzip -uo installed.zip
-	powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\bootstrap-vcpkg.bat'"
-	./vcpkg.exe integrate install;
-	./vcpkg.exe install $VCPKG_LIBS
+	unzip -uo installed.zip > /dev/null
+	./vcpkg.exe integrate install
 else
 	cd /tmp
 	curl --retry 5 --connect-timeout 30 --location --remote-header-name --remote-name https://www.lua.org/ftp/lua-5.3.5.tar.gz
