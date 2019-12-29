d041a2a1f9a3c45177a97079c0f4168d98f125b1
edo9300 <edoardo762@gmail.com>
2019-12-30 00:20:35 +0100

    Add android makefile

---
+++ Android.mk
@@ -0,0 +1,34 @@
+# File: Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := ocgcore_static
+LOCAL_MODULE_FILENAME := libocgcore
+LOCAL_SRC_FILES :=  card.cpp \
+				duel.cpp \
+				effect.cpp \
+				field.cpp \
+				group.cpp \
+				interpreter.cpp \
+				libcard.cpp \
+				libdebug.cpp \
+				libduel.cpp \
+				libeffect.cpp \
+				libgroup.cpp \
+				ocgapi.cpp \
+				operations.cpp \
+				playerop.cpp \
+				processor.cpp \
+				scriptlib.cpp
+                   
+LOCAL_CFLAGS    := -frtti -std=gnu++0x -D_IRR_ANDROID_PLATFORM_ -DLUA_COMPAT_5_2 -Wno-format-security
+LOCAL_CPPFLAGS    := -frtti -std=gnu++0x -D_IRR_ANDROID_PLATFORM_ -DLUA_COMPAT_5_2 -Wno-format-security
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua
+LOCAL_STATIC_LIBRARIES := liblua5.3
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-add-path,$(LOCAL_PATH)/..)
+$(call import-module,lua)
+
