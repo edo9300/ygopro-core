87b9cc57a96007051e7cf0799d4fdf2497c50b96
edo9300 <edoardo762@gmail.com>
2020-04-04 11:49:20 +0200

    Use random engine with forced uint32 sid to ensure cross platform compatibility

--- duel.h
+++ duel.h
@@ -62,7 +62,9 @@ public:
 	std::vector<uint8_t> query_buffer;
 	interpreter* lua;
 	field* game_field;
-	std::mt19937 random;
+	//std::mt19937 uses fast32_t, not ensuring cross platform compatibility
+	std::mersenne_twister_engine<uint32_t, 32, 624, 397, 31, 0x9908b0df,
+		11, 0xffffffff, 7, 0x9d2c5680, 15, 0xefc60000, 18, 1812433253> random;
 	std::unordered_set<card*> cards;
 	std::unordered_set<card*> assumes;
 	std::unordered_set<group*> groups;
