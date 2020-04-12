4f3d4cf423fdcf972392924c79c0a5c02aa9954c
edo9300 <edoardo762@gmail.com>
2020-04-12 13:11:54 +0200

    Revert "Revert "minor refactor of xyz overlay""
    This reverts commit 8ba4ec24ee65f18a07a04e68a6dcd0e441225f11.

--- card.cpp
+++ card.cpp
@@ -1736,64 +1736,52 @@ void card::xyz_overlay(card_set* materials) {
 	if(materials->size() == 0)
 		return;
 	card_set des;
-	if(materials->size() == 1) {
-		card* pcard = *materials->begin();
+	field::card_vector cv;
+	for(auto& pcard : *materials)
+		cv.push_back(pcard);
+	std::sort(cv.begin(), cv.end(), card::card_operation_sort);
+	for(auto& pcard : cv) {
+		if(pcard->overlay_target == this)
+			continue;
+		pcard->current.reason = REASON_XYZ + REASON_MATERIAL;
 		pcard->reset(RESET_LEAVE + RESET_OVERLAY, RESET_EVENT);
 		if(pcard->unique_code)
 			pduel->game_field->remove_unique_card(pcard);
 		if(pcard->equiping_target)
 			pcard->unequip();
-		pcard->clear_card_target();
-		xyz_add(pcard, &des);
-	} else {
-		field::card_vector cv;
-		for(auto& pcard : *materials)
-			cv.push_back(pcard);
-		std::sort(cv.begin(), cv.end(), card::card_operation_sort);
-		for(auto& pcard : cv) {
-			pcard->reset(RESET_LEAVE + RESET_OVERLAY, RESET_EVENT);
-			if(pcard->unique_code)
-				pduel->game_field->remove_unique_card(pcard);
-			if(pcard->equiping_target)
-				pcard->unequip();
-			pcard->clear_card_target();
-			xyz_add(pcard, &des);
+		for(auto cit = pcard->equiping_cards.begin(); cit != pcard->equiping_cards.end();) {
+			card* equipc = *cit++;
+			des.insert(equipc);
+			equipc->unequip();
 		}
+		pcard->clear_card_target();
+		auto message = pduel->new_message(MSG_MOVE);
+		message->write<uint32>(pcard->data.code);
+		message->write(pcard->get_info_location());
+		if(pcard->overlay_target) {
+			pcard->overlay_target->xyz_remove(pcard);
+		} else {
+			pcard->enable_field_effect(false);
+			pduel->game_field->remove_card(pcard);
+			pduel->game_field->add_to_disable_check_list(pcard);
+		}
+		xyz_add(pcard);
+		message->write(pcard->get_info_location());
+		message->write<uint32>(pcard->current.reason);
 	}
 	if(des.size())
 		pduel->game_field->destroy(&des, 0, REASON_LOST_TARGET + REASON_RULE, PLAYER_NONE);
 	else
 		pduel->game_field->adjust_instant();
 }
-void card::xyz_add(card* mat, card_set* des) {
-	if(mat->overlay_target == this)
+void card::xyz_add(card* mat) {
+	if(mat->current.controler != PLAYER_NONE || mat->overlay_target)
 		return;
-	auto message = pduel->new_message(MSG_MOVE);
-	message->write<uint32>(mat->data.code);
-	message->write(mat->get_info_location());
-	if(mat->overlay_target) {
-		mat->overlay_target->xyz_remove(mat);
-	} else {
-		mat->enable_field_effect(false);
-		pduel->game_field->remove_card(mat);
-		pduel->game_field->add_to_disable_check_list(mat);
-	}
-	message->write<uint8>(current.controler);
-	message->write<uint8>(current.location | LOCATION_OVERLAY);
-	message->write<uint32>(current.sequence);
-	message->write<uint32>(current.position);
-	message->write<uint32>(REASON_XYZ + REASON_MATERIAL);
 	xyz_materials.push_back(mat);
-	for(auto cit = mat->equiping_cards.begin(); cit != mat->equiping_cards.end();) {
-		auto rm = cit++;
-		des->insert(*rm);
-		(*rm)->unequip();
-	}
 	mat->overlay_target = this;
 	mat->current.controler = this->current.controler;
 	mat->current.location = LOCATION_OVERLAY;
 	mat->current.sequence = xyz_materials.size() - 1;
-	mat->current.reason = REASON_XYZ + REASON_MATERIAL;
 	for(auto& eit : mat->xmaterial_effect) {
 		effect* peffect = eit.second;
 		if(peffect->type & EFFECT_TYPE_FIELD)
