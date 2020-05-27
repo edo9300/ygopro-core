a522cc25704339320f284c9f0a1b2daaf447226f
lrhel <lrhel@users.noreply.github.com>
2020-05-27 19:28:33 +0200

    Implement DUEL_REPOS_AFTER_CONTROL_SWITCH (#116)

--- processor.cpp
+++ processor.cpp
@@ -2376,6 +2376,7 @@ int32 field::process_idle_command(uint16 step) {
 		} else
 			add_process(PROCESSOR_FLIP_SUMMON, 0, 0, (group*)target, target->current.controler, 0);
 		target->set_status(STATUS_FORM_CHANGED, TRUE);
+		target->set_status(STATUS_CONTROL_CHANGED, FALSE);
 		core.units.begin()->step = -1;
 		return FALSE;
 	}
@@ -2438,6 +2439,7 @@ int32 field::process_idle_command(uint16 step) {
 			add_process(PROCESSOR_POINT_EVENT, 0, 0, 0, FALSE, 0);
 		}
 		target->set_status(STATUS_FORM_CHANGED, TRUE);
+		target->set_status(STATUS_CONTROL_CHANGED, FALSE);
 		core.units.begin()->step = -1;
 		return FALSE;
 	}
