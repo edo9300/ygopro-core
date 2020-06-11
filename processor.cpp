131e87b9ea82f357d3e6261c349955302b820e93
edo9300 <edoardo762@gmail.com>
2020-06-11 23:14:38 +0200

    Process events immediately after battle_confirm if there are forced triggers

--- processor.cpp
+++ processor.cpp
@@ -2937,7 +2937,7 @@ int32 field::process_battle_command(uint16 step) {
 		raise_event((card*)0, EVENT_BATTLE_START, 0, 0, 0, 0, 0);
 		process_single_event();
 		process_instant_event();
-		if(!is_flag(DUEL_6_STEP_BATLLE_STEP)) {
+		if(!is_flag(DUEL_6_STEP_BATLLE_STEP) || (core.new_fchain.size() || core.new_ochain.size())) {
 			message = pduel->new_message(MSG_HINT);
 			message->write<uint8>(HINT_EVENT);
 			message->write<uint8>(0);
