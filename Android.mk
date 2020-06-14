d8741b31a76ca7db585e145fc5343aca184d2917
edo9300 <edoardo762@gmail.com>
2020-06-14 12:46:13 +0200

    Add -fexceptions flag

--- Android.mk
+++ Android.mk
@@ -23,6 +23,7 @@ LOCAL_SRC_FILES :=  card.cpp \
 				scriptlib.cpp
                    
 LOCAL_CFLAGS    := -frtti -DLUA_COMPAT_5_2 -Wno-format-security
+LOCAL_CPPFLAGS := -fexceptions
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua/include
 LOCAL_STATIC_LIBRARIES += liblua5.3
 
