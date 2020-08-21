4947545047a85a86edb1cbdd0b21d62be7ba27ba
edo9300 <edoardo762@gmail.com>
2020-08-21 22:29:45 +0200

    Fix lua_resumec macro for gcc/clang

--- interpreter.h
+++ interpreter.h
@@ -99,7 +99,7 @@ public:
 #define COROUTINE_ERROR		3
 
 #if LUA_VERSION_NUM <= 503
-#define lua_resumec(state, from, nargs) lua_resume(state, from, nargs)
+#define lua_resumec(state, from, nargs, res) lua_resume(state, from, nargs)
 #else
 #define lua_resumec(state, from, nargs, res) lua_resume(state, from, nargs, res)
 #endif
