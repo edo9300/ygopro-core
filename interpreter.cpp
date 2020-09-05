c64e3474553460d3511bfca64f4595f947ea2002
edo9300 <edoardo762@gmail.com>
2020-09-05 20:50:02 +0200

    Use non throwing getter

--- interpreter.cpp
+++ interpreter.cpp
@@ -23,7 +23,7 @@ struct objref {
 	static int32 from_lua_ref(lua_State* L) {
 		auto ref = lua_get<int32>(L, 1);
 		lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
-		auto obj = lua_get<T*, true>(L, -1);
+		auto obj = lua_get<T*>(L, -1);
 		if(!obj) {
 			if(std::is_same<T,card>::value)
 				luaL_error(L, "Parameter 1 should be a lua reference to a Card.");
