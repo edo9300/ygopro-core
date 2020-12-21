07848178fab076691aa3fc75f3278ed13c419f5d
edo9300 <edoardo762@gmail.com>
2020-12-21 17:21:54 +0100

    Update processor.cpp

--- processor.cpp
+++ processor.cpp
@@ -4032,10 +4032,10 @@ int32 field::process_turn(uint16 step, uint8 turn_player) {
 		message->write<uint16>(infos.phase);
 		raise_event((card*)0, EVENT_PREDRAW, 0, 0, 0, turn_player, 0);
 		process_instant_event();
-		pduel->write_buffer8(MSG_HINT);
-		pduel->write_buffer8(HINT_EVENT);
-		pduel->write_buffer8(turn_player);
-		pduel->write_buffer32(27);
+		message = pduel->new_message(MSG_HINT);
+		message->write<uint8_t>(HINT_EVENT);
+		message->write<uint8_t>(turn_player);
+		message->write<uint32_t>(27);
 		if(core.new_fchain.size() || core.new_ochain.size())
 			add_process(PROCESSOR_POINT_EVENT, 0, 0, 0, 0, 0);
 		/*if(core.set_forced_attack)
