ed86d4aaede264b9437904bffad0db2e5e3bf622
Edoardo Lolletti <edoardo762@gmail.com>
2021-05-01 01:37:57 +0200

    Revert "Make grant_effect_container map with effect_sort instead of unordered map"
    This reverts commit 174ec18722ad28cb13fbcc5c3b283bb036301d33.

--- common.h
+++ common.h
@@ -38,9 +38,6 @@ typedef int8_t int8;
 struct card_sort {
 	bool operator()(void* const & c1, void* const & c2) const;
 };
-struct effect_sort {
-	bool operator()(void* const & e1, void* const & e2) const;
-};
 
 //Locations
 #define LOCATION_DECK    0x01
