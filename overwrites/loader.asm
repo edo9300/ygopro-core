21d9106f1a3b6c7f78211fcf0400139eea5419cc
Edoardo Lolletti <edoardo762@gmail.com>
2022-03-27 22:26:51 +0200

    Update winapi overwrites
    Use the updated handlings from client repo

--- overwrites/loader.asm
+++ overwrites/loader.asm
@@ -7,27 +7,28 @@ ENDIF
 END_IF_NOT_X86
 
 .model flat
+.data
 
 ;windows 2000 sp4
 handledInterlockedFlushSList PROTO STDCALL :DWORD
+__imp__InterlockedFlushSList@4 dd handledInterlockedFlushSList
+public __imp__InterlockedFlushSList@4
+
 handledInitializeSListHead PROTO STDCALL :DWORD
+__imp__InitializeSListHead@4 dd handledInitializeSListHead
+public __imp__InitializeSListHead@4
+
 handledGetModuleHandleExW PROTO STDCALL :DWORD,:DWORD,:DWORD
+__imp__GetModuleHandleExW@12 dd handledGetModuleHandleExW
+public __imp__GetModuleHandleExW@12
 
 ;windows xp no service pack
 handledEncodePointer PROTO STDCALL :DWORD
-handledDecodePointer PROTO STDCALL :DWORD
-
-.data
 __imp__EncodePointer@4 dd handledEncodePointer
-__imp__DecodePointer@4 dd handledDecodePointer
-__imp__GetModuleHandleExW@12 dd handledGetModuleHandleExW
-__imp__InitializeSListHead@4 dd handledInitializeSListHead
-__imp__InterlockedFlushSList@4 dd handledInterlockedFlushSList
+public __imp__EncodePointer@4
 
-EXTERNDEF __imp__EncodePointer@4 : DWORD
-EXTERNDEF __imp__DecodePointer@4 : DWORD
-EXTERNDEF __imp__InitializeSListHead@4 : DWORD
-EXTERNDEF __imp__InterlockedFlushSList@4 : DWORD
-EXTERNDEF __imp__GetModuleHandleExW@12 : DWORD
+handledDecodePointer PROTO STDCALL :DWORD
+__imp__DecodePointer@4 dd handledDecodePointer
+public __imp__DecodePointer@4
 
 end
