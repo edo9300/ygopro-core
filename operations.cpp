edo9300/ygopro-core

commit 246da8ad8de8e869847315b3a1787b1f9e2101d3

Remove hardcoded limit on damage/recover

operations.cpp
@@ -134,15 +134,16 @@ void field::equip(uint32_t equip_player, card* equip_card, card* target, uint32_
 void field::draw(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t count) {
 	add_process(PROCESSOR_DRAW, 0, reason_effect, 0, reason, (reason_player << 28) + (playerid << 24) + (count & 0xffffff));
 }
-void field::damage(effect* reason_effect, uint32_t reason, uint32_t reason_player, card* reason_card, uint32_t playerid, uint32_t amount, uint32_t is_step) {
-	uint32_t arg2 = (is_step << 28) + (reason_player << 26) + (playerid << 24) + (amount & 0xffffff);
+void field::damage(effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, bool is_step) {
+	uint32_t arg2 = (is_step << 28) + (reason_player << 26) + (playerid << 24);
 	if(reason & REASON_BATTLE)
-		add_process(PROCESSOR_DAMAGE, 0, (effect*)reason_card, 0, reason, arg2);
+		reason_effect = nullptr;
 	else
-		add_process(PROCESSOR_DAMAGE, 0, reason_effect, 0, reason, arg2);
+		reason_card = nullptr;
+	add_process(PROCESSOR_DAMAGE, 0, reason_effect, 0, reason, arg2, amount, 0, reason_effect);
 }
-void field::recover(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t amount, uint32_t is_step) {
-	add_process(PROCESSOR_RECOVER, 0, reason_effect, 0, reason, (is_step << 28) + (reason_player << 26) + (playerid << 24) + (amount & 0xffffff));
+void field::recover(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t amount, bool is_step) {
+	add_process(PROCESSOR_RECOVER, 0, reason_effect, 0, reason, (is_step << 28) + (reason_player << 26) + (playerid << 24), amount);
 }
 void field::summon(uint32_t sumplayer, card* target, effect* proc, uint32_t ignore_count, uint32_t min_tribute, uint32_t zone) {
 	add_process(PROCESSOR_SUMMON_RULE, 0, proc, (group*)target, sumplayer + (ignore_count << 8) + (min_tribute << 16) + (zone << 24), 0);
@@ -487,12 +488,12 @@ int32_t field::draw(uint16_t step, effect* reason_effect, uint32_t reason, uint8
 	}
 	return TRUE;
 }
-int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, uint32_t is_step) {
+int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, bool is_step) {
 	switch(step) {
 	case 0: {
 		effect_set eset;
-		returns.at<int32_t>(0) = amount;
-		if(amount <= 0)
+		returns.at<uint32_t>(0) = amount;
+		if(amount == 0)
 			return TRUE;
 		if(!(reason & REASON_RDAMAGE)) {
 			filter_player_effect(playerid, EFFECT_REVERSE_DAMAGE, &eset);
@@ -531,12 +532,12 @@ int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uin
 			pduel->lua->add_param<PARAM_TYPE_INT>(reason);
 			pduel->lua->add_param<PARAM_TYPE_INT>(reason_player);
 			pduel->lua->add_param<PARAM_TYPE_CARD>(reason_card);
-			val = peff->get_value(5);
-			returns.at<int32_t>(0) = val;
+			val = static_cast<uint32_t>(peff->get_value(5));
+			returns.at<uint32_t>(0) = val;
 			if(val == 0)
 				return TRUE;
 		}
-		core.units.begin()->arg2 = (core.units.begin()->arg2 & 0xff000000) | (val & 0xffffff);
+		core.units.begin()->arg3 = val;
 		if(is_step) {
 			core.units.begin()->step = 9;
 			return TRUE;
@@ -544,7 +545,7 @@ int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uin
 		return FALSE;
 	}
 	case 1: {
-		uint32_t is_reflect = (core.units.begin()->arg2 >> 29) & 1;
+		bool is_reflect = (core.units.begin()->arg2 >> 29) & 1;
 		if(is_reflect)
 			playerid = 1 - playerid;
 		if(is_reflect || (reason & REASON_RRECOVER))
@@ -570,11 +571,11 @@ int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uin
 		return FALSE;
 	}
 	case 2: {
-		returns.at<int32_t>(0) = amount;
+		returns.at<uint32_t>(0) = amount;
 		return TRUE;
 	}
 	case 3: {
-		returns.at<int32_t>(0) = 0;
+		returns.at<uint32_t>(0) = 0;
 		return TRUE;
 	}
 	case 10: {
@@ -584,12 +585,12 @@ int32_t field::damage(uint16_t step, effect* reason_effect, uint32_t reason, uin
 	}
 	return TRUE;
 }
-int32_t field::recover(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, uint8_t playerid, uint32_t amount, uint32_t is_step) {
+int32_t field::recover(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, uint8_t playerid, uint32_t amount, bool is_step) {
 	switch(step) {
 	case 0: {
 		effect_set eset;
-		returns.at<int32_t>(0) = amount;
-		if(amount <= 0)
+		returns.at<uint32_t>(0) = amount;
+		if(amount == 0)
 			return TRUE;
 		if(!(reason & REASON_RRECOVER)) {
 			filter_player_effect(playerid, EFFECT_REVERSE_RECOVER, &eset);
@@ -623,11 +624,11 @@ int32_t field::recover(uint16_t step, effect* reason_effect, uint32_t reason, ui
 		return FALSE;
 	}
 	case 2: {
-		returns.at<int32_t>(0) = amount;
+		returns.at<uint32_t>(0) = amount;
 		return TRUE;
 	}
 	case 3: {
-		returns.at<int32_t>(0) = 0;
+		returns.at<uint32_t>(0) = 0;
 		return TRUE;
 	}
 	case 10: {

processor.cpp
@@ -458,14 +458,18 @@ int32_t field::process() {
 		return PROCESSOR_FLAG_CONTINUE;
 	}
 	case PROCESSOR_DAMAGE: {
-		int32_t reason = it->arg1;
-		effect* reason_effect = 0;
-		card* reason_card = 0;
+		uint32_t reason = static_cast<uint32_t>(it->arg1);
+		effect* reason_effect = nullptr;
+		card* reason_card = nullptr;
 		if(reason & REASON_BATTLE)
 			reason_card = (card*)it->peffect;
 		else
 			reason_effect = it->peffect;
-		if (damage(it->step, reason_effect, reason, (it->arg2 >> 26) & 0x3, reason_card, (it->arg2 >> 24) & 0x3, it->arg2 & 0xffffff, (it->arg2 >> 28) & 0x1)) {
+		uint32_t amount = static_cast<uint32_t>(it->arg3);
+		uint8_t reason_player = (it->arg2 >> 26) & 0x3;
+		uint8_t playerid = (it->arg2 >> 24) & 0x3;
+		bool is_step = (it->arg2 >> 28) & 0x1;
+		if (damage(it->step, reason_effect, reason, reason_player, reason_card, playerid, amount, is_step)) {
 			if(it->step == 9) {
 				it->step = 1;
 				core.recover_damage_reserve.splice(core.recover_damage_reserve.end(), core.units, it);
@@ -476,7 +480,12 @@ int32_t field::process() {
 		return PROCESSOR_FLAG_CONTINUE;
 	}
 	case PROCESSOR_RECOVER: {
-		if (recover(it->step, it->peffect, it->arg1, (it->arg2 >> 26) & 0x3, (it->arg2 >> 24) & 0x3, it->arg2 & 0xffffff, (it->arg2 >> 28) & 0x1)) {
+		uint32_t reason = static_cast<uint32_t>(it->arg1);
+		uint8_t reason_player = (it->arg2 >> 26) & 0x3;
+		uint8_t playerid = (it->arg2 >> 24) & 0x3;
+		uint32_t amount = static_cast<uint32_t>(it->arg3);
+		bool is_step = (it->arg2 >> 28) & 0x1;
+		if (recover(it->step, it->peffect, reason, reason_player, playerid, amount, is_step)) {
 			if(it->step == 9) {
 				it->step = 1;
 				core.recover_damage_reserve.splice(core.recover_damage_reserve.end(), core.units, it);

field.h
@@ -609,8 +609,8 @@ public:
 	void swap_control(effect* reason_effect, uint32_t reason_player, card* pcard1, card* pcard2, uint32_t reset_phase, uint32_t reset_count);
 	void equip(uint32_t equip_player, card* equip_card, card* target, uint32_t up, uint32_t is_step);
 	void draw(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t count);
-	void damage(effect* reason_effect, uint32_t reason, uint32_t reason_player, card* reason_card, uint32_t playerid, uint32_t amount, uint32_t is_step = FALSE);
-	void recover(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t amount, uint32_t is_step = FALSE);
+	void damage(effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, bool is_step = false);
+	void recover(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t amount, bool is_step = false);
 	void summon(uint32_t sumplayer, card* target, effect* proc, uint32_t ignore_count, uint32_t min_tribute, uint32_t zone = 0x1f);
 	void mset(uint32_t setplayer, card* target, effect* proc, uint32_t ignore_count, uint32_t min_tribute, uint32_t zone = 0x1f);
 	void special_summon_rule(uint32_t sumplayer, card* target, uint32_t summon_type);
@@ -638,8 +638,8 @@ public:
 	int32_t self_destroy(uint16_t step, card* ucard, int32_t p);
 	int32_t equip(uint16_t step, uint8_t equip_player, card* equip_card, card* target, uint32_t up, uint32_t is_step);
 	int32_t draw(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, uint8_t playerid, uint32_t count);
-	int32_t damage(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, uint32_t is_step);
-	int32_t recover(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, uint8_t playerid, uint32_t amount, uint32_t is_step);
+	int32_t damage(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, card* reason_card, uint8_t playerid, uint32_t amount, bool is_step);
+	int32_t recover(uint16_t step, effect* reason_effect, uint32_t reason, uint8_t reason_player, uint8_t playerid, uint32_t amount, bool is_step);
 	int32_t summon(uint16_t step, uint8_t sumplayer, card* target, effect* proc, uint8_t ignore_count, uint8_t min_tribute, uint32_t zone);
 	int32_t flip_summon(uint16_t step, uint8_t sumplayer, card* target);
 	int32_t mset(uint16_t step, uint8_t setplayer, card* ptarget, effect* proc, uint8_t ignore_count, uint8_t min_tribute, uint32_t zone);

libduel.cpp
@@ -1189,15 +1189,16 @@ int32_t duel_damage(lua_State* L) {
 	auto playerid = lua_get<uint8_t>(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	auto amount = lua_get<int32_t>(L, 2);
-	if(amount < 0)
-		amount = 0;
+	auto amount = lua_get<int64_t>(L, 2);
+	uint32_t actual_amount = 0;
+	if(amount > 0)
+		actual_amount = static_cast<uint32_t>(amount);
 	auto reason = lua_get<uint32_t>(L, 3);
 	bool is_step = lua_get<bool, false>(L, 4);
 	const auto pduel = lua_get<duel*>(L);
-	pduel->game_field->damage(pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, 0, playerid, amount, is_step);
+	pduel->game_field->damage(pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, 0, playerid, actual_amount, is_step);
 	return lua_yieldk(L, 0, 0, [](lua_State* L, int32_t/* status*/, lua_KContext/* ctx*/) {
-		lua_pushinteger(L, lua_get<duel*>(L)->game_field->returns.at<int32_t>(0));
+		lua_pushinteger(L, lua_get<duel*>(L)->game_field->returns.at<uint32_t>(0));
 		return 1;
 	});
 }
@@ -1207,15 +1208,16 @@ int32_t duel_recover(lua_State* L) {
 	auto playerid = lua_get<uint8_t>(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	auto amount = lua_get<int32_t>(L, 2);
-	if(amount < 0)
-		amount = 0;
+	auto amount = lua_get<int64_t>(L, 2);
+	uint32_t actual_amount = 0;
+	if(amount > 0)
+		actual_amount = static_cast<uint32_t>(amount);
 	auto reason = lua_get<uint32_t>(L, 3);
 	bool is_step = lua_get<bool, false>(L, 4);
 	const auto pduel = lua_get<duel*>(L);
-	pduel->game_field->recover(pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, playerid, amount, is_step);
+	pduel->game_field->recover(pduel->game_field->core.reason_effect, reason, pduel->game_field->core.reason_player, playerid, actual_amount, is_step);
 	return lua_yieldk(L, 0, 0, [](lua_State* L, int32_t/* status*/, lua_KContext/* ctx*/) {
-		lua_pushinteger(L, lua_get<duel*>(L)->game_field->returns.at<int32_t>(0));
+		lua_pushinteger(L, lua_get<duel*>(L)->game_field->returns.at<uint32_t>(0));
 		return 1;
 	});
 }

commit 16da613742fa7cf1dd58862174cbd501433bfd25

Fix typo that caused EVENT_BATTLE_DAMAGE not be raised
Issue introduced in 246da8ad8de8e869847315b3a1787b1f9e2101d3

operations.cpp
@@ -140,7 +140,7 @@ void field::damage(effect* reason_effect, uint32_t reason, uint8_t reason_player
 		reason_effect = nullptr;
 	else
 		reason_card = nullptr;
-	add_process(PROCESSOR_DAMAGE, 0, reason_effect, 0, reason, arg2, amount, 0, reason_effect);
+	add_process(PROCESSOR_DAMAGE, 0, reason_effect, 0, reason, arg2, amount, 0, reason_card);
 }
 void field::recover(effect* reason_effect, uint32_t reason, uint32_t reason_player, uint32_t playerid, uint32_t amount, bool is_step) {
 	add_process(PROCESSOR_RECOVER, 0, reason_effect, 0, reason, (is_step << 28) + (reason_player << 26) + (playerid << 24), amount);

processor.cpp
@@ -462,7 +462,7 @@ int32_t field::process() {
 		effect* reason_effect = nullptr;
 		card* reason_card = nullptr;
 		if(reason & REASON_BATTLE)
-			reason_card = (card*)it->peffect;
+			reason_card = static_cast<card*>(it->ptr1);
 		else
 			reason_effect = it->peffect;
 		uint32_t amount = static_cast<uint32_t>(it->arg3);