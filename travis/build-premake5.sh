a902b3a651ec25d42230b1faf3502eaf77858c70
Edoardo Lolletti <edoardo762@gmail.com>
2022-04-27 19:24:20 +0200

    Use premake to generate core libraries

--- travis/build-premake5.sh
+++ travis/build-premake5.sh
@@ -3,8 +3,8 @@
 set -euxo pipefail
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-	./premake5.exe vs2017;
-    "$MSBUILD_PATH/msbuild.exe" -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ocgcore.sln;
+	./premake5.exe vs2019 --oldwindows=true;
+	msbuild.exe -m -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 ./build/ocgcore.sln;
 else
 	./premake5 gmake2;
 	make -Cbuild -j2 config=$BUILD_CONFIG;
