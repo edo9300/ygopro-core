edo9300/ygopro-core

commit 3caf6a957ab6d5191c96216a4dc8769cb3227428

fix all warnings under linux, enable all warnings (#28)
fixes all warnings on GCC 7.3.0. Not tested
using any other compile yet, so warnings should
still be expected using different compilers.

enables all warnings for all platforms, activates
'-pedantic' mode as well as updates the c++ standard
flag for non windows compilers.

note: This commit makes the compiler ignore
the '-Wunused-parameter' warning.

card.cpp
@@ -1085,14 +1085,14 @@ uint32 card::get_link() {
 		return data.level;
 	if (temp.level != 0xffffffff)
 		return temp.level;
-		effect_set effects;
-		int32 link = data.level;
-		temp.level = link;
-		int32 up = 0, upc = 0;
-		filter_effect(EFFECT_UPDATE_LINK, &effects, FALSE);
-		filter_effect(EFFECT_CHANGE_LINK, &effects, FALSE);
-		filter_effect(EFFECT_CHANGE_LINK_FINAL, &effects);
-		for (int32 i = 0; i < effects.size(); ++i) {
+	effect_set effects;
+	int32 link = data.level;
+	temp.level = link;
+	int32 up = 0, upc = 0;
+	filter_effect(EFFECT_UPDATE_LINK, &effects, FALSE);
+	filter_effect(EFFECT_CHANGE_LINK, &effects, FALSE);
+	filter_effect(EFFECT_CHANGE_LINK_FINAL, &effects);
+	for (int32 i = 0; i < effects.size(); ++i) {
 		switch (effects[i]->code) {
 		case EFFECT_UPDATE_LINK:
 			if ((effects[i]->type & EFFECT_TYPE_SINGLE) && !effects[i]->is_flag(EFFECT_FLAG_SINGLE_RANGE))
@@ -1119,7 +1119,7 @@ uint32 card::get_link() {
 	return link;		
 }
 uint32 card::get_synchro_level(card* pcard) {
-	if(((data.type & TYPE_XYZ) || (status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S)))
+	if(((data.type & TYPE_XYZ) || ((status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S))))
 	|| (data.type & TYPE_LINK))
 		return 0;
 	uint32 lev;
@@ -1132,7 +1132,7 @@ uint32 card::get_synchro_level(card* pcard) {
 	return lev;
 }
 uint32 card::get_ritual_level(card* pcard) {
-	if(((data.type & TYPE_XYZ) || (status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S)))
+	if(((data.type & TYPE_XYZ) || ((status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S))))
 	|| (data.type & TYPE_LINK))
 		return 0;
 	uint32 lev;
@@ -1668,7 +1668,7 @@ uint32 card::get_column_zone(int32 loc1, int32 left, int32 right) {
 	int32 zones = 0;
 	int32 loc2 = current.location;
 	int32 s = current.sequence;
-	if(!(loc1 & LOCATION_ONFIELD) || !(loc2 & LOCATION_ONFIELD) || loc2 == LOCATION_SZONE && s >=5 || left < 0 || right < 0)
+	if(!(loc1 & LOCATION_ONFIELD) || !(loc2 & LOCATION_ONFIELD) || (loc2 == LOCATION_SZONE && s >= 5) || left < 0 || right < 0)
 		return 0;
 	if(s <= 4) {
 		if(loc1 != loc2)
@@ -1736,7 +1736,7 @@ int32 card::is_all_column() {
 		return FALSE;
 	card_set cset;
 	get_column_cards(&cset, 0, 0);
-	int32 full = 3;
+	uint32 full = 3;
 	if((pduel->game_field->core.duel_options & DUEL_EMZONE) && (current.sequence == 1 || current.sequence == 3))
 		full++;
 	if(cset.size() == full)
@@ -2842,11 +2842,13 @@ int32 card::check_set_procedure(effect* peffect, uint8 playerid, uint8 ignore_co
 		return FALSE;
 	if(!pduel->game_field->is_player_can_mset(peffect->get_value(this), playerid, this))
 		return FALSE;
+	/*
 	uint8 toplayer = playerid;
 	if(peffect->is_flag(EFFECT_FLAG_SPSUM_PARAM)) {
 		if(peffect->o_range)
 			toplayer = 1 - playerid;
 	}
+	*/ // UNUSED VARIABLE
 	if(!ignore_count && !pduel->game_field->core.extra_summon[playerid]
 			&& pduel->game_field->core.summon_count[playerid] >= pduel->game_field->get_summon_count_limit(playerid)) {
 		effect_set eset;
@@ -2990,7 +2992,7 @@ effect* card::is_affected_by_effect(int32 code, card* target) {
 	}
 	return 0;
 }
-int32 card::get_card_effect(int32 code) {
+int32 card::get_card_effect(uint32 code) {
 	effect* peffect;
 	int32 i = 0;
 	for (auto rg = single_effect.begin(); rg != single_effect.end(); ++rg) {
@@ -3592,7 +3594,7 @@ int32 card::is_destructable_by_effect(effect* peffect, uint8 playerid) {
 			pduel->lua->add_param(REASON_EFFECT, PARAM_TYPE_INT);
 			pduel->lua->add_param(playerid, PARAM_TYPE_INT);
 			int32 ct;
-			if(ct = eset[i]->get_value(3)) {
+			if((ct = eset[i]->get_value(3))) {
 				auto it = indestructable_effects.insert(std::make_pair(eset[i]->id, 0));
 				if(it.first->second + 1 <= ct) {
 					return FALSE;

card.h
@@ -286,7 +286,7 @@ public:
 	void filter_spsummon_procedure_g(uint8 playerid, effect_set* eset);
 	effect* is_affected_by_effect(int32 code);
 	effect* is_affected_by_effect(int32 code, card* target);
-	int32 get_card_effect(int32 code);
+	int32 get_card_effect(uint32 code);
 	effect* check_control_effect();
 	int32 fusion_check(group* fusion_m, group* cg, uint32 chkf);
 	void fusion_filter_valid(group* fusion_m, group* cg, uint32 chkf, effect_set* eset);

effect.cpp
@@ -13,7 +13,7 @@
 
 bool effect_sort_id(const effect* e1, const effect* e2) {
 	return e1->id < e2->id;
-};
+}
 effect::effect(duel* pd) {
 	scrtype = 3;
 	ref_handle = 0;

field.cpp
@@ -219,7 +219,7 @@ void field::add_card(uint8 playerid, card* pcard, uint8 location, uint8 sequence
 		break;
 	}
 	case LOCATION_EXTRA: {
-		if(player[playerid].extra_p_count == 0 || (pcard->data.type & TYPE_PENDULUM) && (pcard->sendto_param.position & POS_FACEUP))
+		if(player[playerid].extra_p_count == 0 || ((pcard->data.type & TYPE_PENDULUM) && (pcard->sendto_param.position & POS_FACEUP)))
 			player[playerid].list_extra.push_back(pcard);
 		else
 			player[playerid].list_extra.insert(player[playerid].list_extra.end() - player[playerid].extra_p_count, pcard);
@@ -2343,7 +2343,7 @@ int32 field::get_attack_target(card* pcard, card_vector* v, uint8 chain_attack) 
 					attack_tg.push_back(atarget);
 			}
 		}
-		if(is_player_affected_by_effect(p, EFFECT_SELF_ATTACK) && (!pcard->is_affected_by_effect(EFFECT_ATTACK_ALL) || !&attack_tg)) {
+		if(is_player_affected_by_effect(p, EFFECT_SELF_ATTACK) && (!pcard->is_affected_by_effect(EFFECT_ATTACK_ALL) || !attack_tg.size())) {
 			for (uint32 i = 0; i < 7; ++i) {
 				card* atarget = player[p].list_mzone[i];
 				if (atarget != core.attacker) {
@@ -2486,7 +2486,7 @@ bool field::confirm_attack_target() {
 	uint8 p = pcard->current.controler;
 	effect* peffect;
 	card_vector* pv = NULL;
-	int32 atype = 0;
+	//int32 atype = 0; // UNUSED VARIABLE
 	card_vector must_be_attack;
 	card_vector attack_tg;
 	card_vector only_be_attack;
@@ -2504,7 +2504,7 @@ bool field::confirm_attack_target() {
 	}
 	pcard->filter_effect(EFFECT_RISE_TO_FULL_HEIGHT, &eset);
 	if(eset.size()) {
-		atype = 1;
+		//atype = 1; // UNUSED VARIABLE
 		std::set<uint32> idset;
 		for(int32 i = 0; i < eset.size(); ++i)
 			idset.insert(eset[i]->label);
@@ -2513,26 +2513,26 @@ bool field::confirm_attack_target() {
 		else
 			return false;
 	} else if(pcard->is_affected_by_effect(EFFECT_ONLY_ATTACK_MONSTER)) {
-		atype = 2;
+		//atype = 2; // UNUSED VARIABLE
 		if(only_be_attack.size() == 1)
 			pv = &only_be_attack;
 		else
 			return false;
 	} else if(pcard->is_affected_by_effect(EFFECT_MUST_ATTACK_MONSTER)) {
-		atype = 3;
+		//atype = 3; // UNUSED VARIABLE
 		if(must_be_attack.size())
 			pv = &must_be_attack;
 		else
 			return false;
 	} else {
-		atype = 4;
+		//atype = 4; // UNUSED VARIABLE
 		for (uint32 i = 0; i < 7; ++i) {
 			card* atarget = player[1 - p].list_mzone[i];
 			if (atarget != core.attacker) {
 				attack_tg.push_back(atarget);
 			}
 		}
-		if(is_player_affected_by_effect(p, EFFECT_SELF_ATTACK) && (!pcard->is_affected_by_effect(EFFECT_ATTACK_ALL) || !&attack_tg)) {
+		if(is_player_affected_by_effect(p, EFFECT_SELF_ATTACK) && (!pcard->is_affected_by_effect(EFFECT_ATTACK_ALL) || !attack_tg.size())) {
 			for (uint32 i = 0; i < 7; ++i) {
 				card* atarget = player[p].list_mzone[i];
 				if (atarget != core.attacker) {
@@ -3024,7 +3024,7 @@ int32 field::is_player_can_remove(uint8 playerid, card * pcard) {
 }
 int32 field::is_chain_negatable(uint8 chaincount) {
 	effect_set eset;
-	if(chaincount < 0 || chaincount > core.current_chain.size())
+	if(chaincount > core.current_chain.size())
 		return FALSE;
 	effect* peffect;
 	if(chaincount == 0)
@@ -3043,7 +3043,7 @@ int32 field::is_chain_negatable(uint8 chaincount) {
 }
 int32 field::is_chain_disablable(uint8 chaincount) {
 	effect_set eset;
-	if(chaincount < 0 || chaincount > core.current_chain.size())
+	if(chaincount > core.current_chain.size())
 		return FALSE;
 	effect* peffect;
 	if(chaincount == 0)
@@ -3063,7 +3063,7 @@ int32 field::is_chain_disablable(uint8 chaincount) {
 	return TRUE;
 }
 int32 field::is_chain_disabled(uint8 chaincount) {
-	if(chaincount < 0 || chaincount > core.current_chain.size())
+	if(chaincount > core.current_chain.size())
 		return FALSE;
 	chain* pchain;
 	if(chaincount == 0)
@@ -3084,7 +3084,7 @@ int32 field::is_chain_disabled(uint8 chaincount) {
 	return FALSE;
 }
 int32 field::check_chain_target(uint8 chaincount, card * pcard) {
-	if(chaincount < 0 || chaincount > core.current_chain.size())
+	if(chaincount > core.current_chain.size())
 		return FALSE;
 	chain* pchain;
 	if(chaincount == 0)
@@ -3140,7 +3140,7 @@ int32 field::get_cteffect(effect* peffect, int32 playerid, int32 store) {
 		if(!feffect->in_range(phandler))
 			continue;
 		uint32 code = efit->first;
-		if(code == EVENT_FREE_CHAIN || code == EVENT_PHASE + infos.phase) {
+		if(code == EVENT_FREE_CHAIN || code == EVENT_PHASE + (uint32)infos.phase) {
 			nil_event.event_code = code;
 			if(get_cteffect_evt(feffect, playerid, nil_event, store) && !store)
 				return TRUE;

interpreter.cpp
@@ -845,7 +845,7 @@ int32 interpreter::call_function(int32 f, uint32 param_count, int32 ret_count) {
 		return OPERATION_FAIL;
 	}
 	if (param_count != params.size()) {
-		sprintf(pduel->strbuffer, "\"CallFunction\": incorrect parameter count (%d expected, %ud pushed)", param_count, params.size());
+		sprintf(pduel->strbuffer, "\"CallFunction\": incorrect parameter count (%d expected, %ud pushed)", param_count, (uint32)params.size());
 		handle_message(pduel, 1);
 		params.clear();
 		return OPERATION_FAIL;

libduel.cpp
@@ -1522,8 +1522,8 @@ int32 scriptlib::duel_change_attack_target(lua_State *L) {
 	field::card_vector cv;
 	pduel->game_field->get_attack_target(attacker, &cv, pduel->game_field->core.chain_attack);
 	auto turnp = pduel->game_field->infos.turn_player;
-	if((target && std::find(cv.begin(), cv.end(), target) != cv.end() || ignore)
-			|| !target && !attacker->is_affected_by_effect(EFFECT_CANNOT_DIRECT_ATTACK)) {
+	if(((target && std::find(cv.begin(), cv.end(), target) != cv.end()) || ignore) ||
+		(!target && !attacker->is_affected_by_effect(EFFECT_CANNOT_DIRECT_ATTACK))) {
 		pduel->game_field->core.attack_target = target;
 		pduel->game_field->core.attack_rollback = FALSE;
 		pduel->game_field->core.opp_mzone.clear();

libgroup.cpp
@@ -123,7 +123,7 @@ int32 scriptlib::group_take_at_pos(lua_State *L) {
 	check_param(L, PARAM_TYPE_GROUP, 1);
 	group* pgroup = *(group**)lua_touserdata(L, 1);
 	int32 pos = lua_tonumberint(L, 2);
-	if(pos > pgroup->container.size())
+	if(pos > (int32)pgroup->container.size())
 		lua_pushnil(L);
 	else {
 		auto cit = pgroup->container.begin();

ocgapi.cpp
@@ -44,7 +44,7 @@ byte* default_script_reader(const char* script_name, int* slen) {
 	fp = fopen(script_name, "rb");
 	if (!fp)
 		return 0;
-	int len = fread(buffer, 1, sizeof(buffer), fp);
+	size_t len = fread(buffer, 1, sizeof(buffer), fp);
 	fclose(fp);
 	if(len >= sizeof(buffer))
 		return 0;
@@ -108,8 +108,8 @@ extern "C" DECL_DLLEXPORT void start_duel(ptr pduel, int32 options) {
 	}
 	if (options & DUEL_RELAY_MODE) {
 		for (int p = 0; p < 2; p++)
-			for (int l = 0; l < pd->game_field->player[p].relay_list_main.size(); l++)
-				for (int i = 0; i < pd->game_field->player[p].start_count && pd->game_field->player[p].relay_list_main[l].size(); ++i) {
+			for (size_t l = 0; l < pd->game_field->player[p].relay_list_main.size(); l++)
+				for (size_t i = 0; i < (size_t)pd->game_field->player[p].start_count && pd->game_field->player[p].relay_list_main[l].size(); ++i) {
 					if (pd->game_field->player[p].relay_list_hand.size() == l)
 						pd->game_field->player[p].relay_list_hand.push_back(std::vector<card*>());
 					card* pcard = pd->game_field->player[p].relay_list_main[l].back();

commit 693c343afb917f790edfbc391573d2d70b138299

Fix GetSynchroLevel and GetRitualLevel with xyz monsters
Regression introduced in https://github.com/edo9300/ygopro-core/commit/3caf6a957ab6d5191c96216a4dc8769cb3227428#diff-f069abeffd0316f0fefbba4485b0ac192416f9ce637219e66bd52ab15cd7d494L1122
went unnoticed as this combination is very rare

card.cpp
@@ -1163,8 +1163,10 @@ uint32_t card::get_link() {
 	return link;		
 }
 uint32_t card::get_synchro_level(card* pcard) {
-	if(((data.type & TYPE_XYZ) || ((status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S))))
-	|| (data.type & TYPE_LINK))
+	if(data.type & TYPE_LINK)
+		return 0;
+	if(((data.type & TYPE_XYZ) || (status & STATUS_NO_LEVEL))
+		&& !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S)))
 		return 0;
 	uint32_t lev;
 	effect_set eset;
@@ -1176,8 +1178,10 @@ uint32_t card::get_synchro_level(card* pcard) {
 	return lev;
 }
 uint32_t card::get_ritual_level(card* pcard) {
-	if(((data.type & TYPE_XYZ) || ((status & STATUS_NO_LEVEL) && !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S))))
-	|| (data.type & TYPE_LINK))
+	if(data.type & TYPE_LINK)
+		return 0;
+	if(((data.type & TYPE_XYZ) || (status & STATUS_NO_LEVEL))
+		&& !(is_affected_by_effect(EFFECT_RANK_LEVEL) || is_affected_by_effect(EFFECT_RANK_LEVEL_S)))
 		return 0;
 	uint32_t lev;
 	effect_set eset;