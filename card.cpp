edo9300/ygopro-core

commit cd4c22522abdcb3b20e4ad81b4dc3ce827d0deb1

Check for STATUS_PROC_COMPLETE on cards currently attached to other cards (#122)
Makes the checks for special summonin validity not always return true if the card has a summon condition and is currently attached to another card.

card.cpp
@@ -3545,6 +3545,8 @@ int32_t card::is_can_be_special_summoned(effect* reason_effect, uint32_t sumtype
 		if(!nolimit && (data.type & TYPE_PENDULUM) && current.location == LOCATION_EXTRA && (current.position & POS_FACEUP))
 			return FALSE;
 	}
+	if(current.location == LOCATION_OVERLAY && !is_status(STATUS_PROC_COMPLETE))
+		return FALSE;
 	if((data.type & TYPE_PENDULUM) && current.location == LOCATION_EXTRA && (current.position & POS_FACEUP)
 		&& (sumtype == SUMMON_TYPE_FUSION || sumtype == SUMMON_TYPE_SYNCHRO || sumtype == SUMMON_TYPE_XYZ))
 		return FALSE;

commit 5aa71a9d9733777c28089b131ecef5d14023866e

Fix regression introduced in https://github.com/edo9300/ygopro-core/pull/122
The above change made ALL xyz materials be checked for being properly summoned when checking is_can_be_special_summoned instead of only materials with a revive limit effect, thus returning false for all the cards that didn't have such effect

card.cpp
@@ -3574,15 +3574,15 @@ int32_t card::is_can_be_special_summoned(effect* reason_effect, uint32_t sumtype
 		return FALSE;
 	if(current.location == LOCATION_REMOVED && (current.position & POS_FACEDOWN))
 		return FALSE;
-	if(is_affected_by_effect(EFFECT_REVIVE_LIMIT) && !is_status(STATUS_PROC_COMPLETE)) {
-		if((!nolimit && (current.location & (LOCATION_GRAVE | LOCATION_REMOVED | LOCATION_SZONE)))
-			|| (!nocheck && !nolimit && (current.location & (LOCATION_DECK | LOCATION_HAND))))
+	if(!nolimit && is_affected_by_effect(EFFECT_REVIVE_LIMIT) && !is_status(STATUS_PROC_COMPLETE)) {
+		if((current.location & (LOCATION_GRAVE | LOCATION_REMOVED | LOCATION_SZONE))
+			|| (!nocheck && (current.location & (LOCATION_DECK | LOCATION_HAND))))
 			return FALSE;
-		if(!nolimit && (data.type & TYPE_PENDULUM) && current.location == LOCATION_EXTRA && (current.position & POS_FACEUP))
+		if((data.type & TYPE_PENDULUM) && current.location == LOCATION_EXTRA && (current.position & POS_FACEUP))
+			return FALSE;
+		if(current.location == LOCATION_OVERLAY)
 			return FALSE;
 	}
-	if(current.location == LOCATION_OVERLAY && !is_status(STATUS_PROC_COMPLETE))
-		return FALSE;
 	if((data.type & TYPE_PENDULUM) && current.location == LOCATION_EXTRA && (current.position & POS_FACEUP)
 		&& (sumtype == SUMMON_TYPE_FUSION || sumtype == SUMMON_TYPE_SYNCHRO || sumtype == SUMMON_TYPE_XYZ))
 		return FALSE;