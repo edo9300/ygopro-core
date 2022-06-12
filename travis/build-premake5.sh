58bd8ac1aef8cbfd3aee8266c85be4fc396ec053
Edoardo Lolletti <edoardo762@gmail.com>
2022-06-12 17:14:10 +0200

    build with j3 on mac os

--- travis/build-premake5.sh
+++ travis/build-premake5.sh
@@ -5,7 +5,12 @@ set -euxo pipefail
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 	./premake5.exe vs2019 --oldwindows=true;
 	msbuild.exe -p:Configuration=$BUILD_CONFIG -p:Platform=Win32 -t:ocgcoreshared  -verbosity:minimal -p:EchoOff=true ./build/ocgcore.sln;
-else
-	./premake5 gmake2;
+fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    ./premake5 gmake2
 	make -Cbuild ocgcoreshared -j2 config=$BUILD_CONFIG;
+fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    ./premake5 gmake2
+	make -Cbuild ocgcoreshared -j3 config=$BUILD_CONFIG;
 fi
