e061ed60338d5f95596c75a9befbe304b3dfbb00
Edoardo Lolletti <edoardo762@gmail.com>
2022-06-22 13:06:29 +0200

    Build lua with j3 on mac os

--- travis/install-lua.sh
+++ travis/install-lua.sh
@@ -21,7 +21,7 @@ else
 	tar xf lua-5.3.5.tar.gz
 	cd lua-5.3.5
 	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-	  make -j2 macosx CC=$CXX MYCFLAGS="${CFLAGS:-""}" MYLDFLAGS="${LDFLAGS:-""}"
+	  make -j3 macosx CC=$CXX MYCFLAGS="${CFLAGS:-""}" MYLDFLAGS="${LDFLAGS:-""}"
 	else
 	  make -j2 posix CC=$CXX MYCFLAGS=-fPIC
 	fi
