e4fc5318a8ff0d88e20283ea738f87f28e16e7e7
Edoardo Lolletti <edoardo762@gmail.com>
2022-10-05 19:53:22 +0200

    Silence extra ‘;’ warning

--- group.h
+++ group.h
@@ -28,14 +28,14 @@ public:
 		return container.find(c) != container.end();
 	}
 	
-	explicit group(duel* pd) : lua_obj_helper(pd) {};
-	group(duel* pd, card* pcard) : lua_obj_helper(pd), container({ pcard }) {};
-	group(duel* pd, const card_set& cset) : lua_obj_helper(pd), container(cset) {};
-	group(duel* pd, card_set&& cset) : lua_obj_helper(pd), container(std::move(cset)) {};
-	group(duel* pd, group* pgroup) : lua_obj_helper(pd), container(pgroup->container) {};
+	explicit group(duel* pd) : lua_obj_helper(pd) {}
+	group(duel* pd, card* pcard) : lua_obj_helper(pd), container({ pcard }) {}
+	group(duel* pd, const card_set& cset) : lua_obj_helper(pd), container(cset) {}
+	group(duel* pd, card_set&& cset) : lua_obj_helper(pd), container(std::move(cset)) {}
+	group(duel* pd, group* pgroup) : lua_obj_helper(pd), container(pgroup->container) {}
 	template<typename Iter>
-	group(duel* pd, const Iter begin, const Iter end) : lua_obj_helper(pd), container(begin, end) {};
-	group(duel* pd, const std::vector<card*>& vcard) : group(pd, vcard.begin(), vcard.end()) {};
+	group(duel* pd, const Iter begin, const Iter end) : lua_obj_helper(pd), container(begin, end) {}
+	group(duel* pd, const std::vector<card*>& vcard) : group(pd, vcard.begin(), vcard.end()) {}
 	group(duel* pd, lua_obj* pobj);
 	~group() = default;
 };
