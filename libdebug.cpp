edo9300/ygopro-core

commit 99361da960d421338936c6a3c27d79e1fffc7036

Fix some places where the offseted sequences weren't being handled

libdebug.cpp
@@ -52,16 +52,28 @@ LUA_FUNCTION(AddCard) {
 		if(location == LOCATION_EXTRA && (position == 0 || (pcard->data.type & TYPE_PENDULUM) == 0))
 			position = POS_FACEDOWN_DEFENSE;
 		pcard->sendto_param.position = position;
+		bool pzone = false;
 		if(location == LOCATION_PZONE) {
-			int32_t seq = field->get_pzone_index(sequence, playerid);
-			field->add_card(playerid, pcard, LOCATION_SZONE, seq, TRUE);
+			location = LOCATION_SZONE;
+			sequence = field->get_pzone_index(sequence, playerid);
 		} else if(location == LOCATION_FZONE) {
-			int32_t loc = LOCATION_SZONE;
-			field->add_card(playerid, pcard, loc, 5);
-		} else
-			field->add_card(playerid, pcard, location, sequence);
+			location = LOCATION_SZONE;
+			sequence = 5;
+		} else if(location == LOCATION_STZONE) {
+			location = LOCATION_SZONE;
+			sequence += 1 * pduel->game_field->is_flag(DUEL_3_COLUMNS_FIELD);
+
+		} else if(location == LOCATION_MMZONE) {
+			location = LOCATION_MZONE;
+			sequence += 1 * pduel->game_field->is_flag(DUEL_3_COLUMNS_FIELD);
+
+		} else if(location == LOCATION_EMZONE) {
+			location = LOCATION_MZONE;
+			sequence += 5;
+		}
+		field->add_card(playerid, pcard, location, sequence, pzone);
 		pcard->current.position = position;
-		if(!(location & (LOCATION_ONFIELD | LOCATION_PZONE | LOCATION_FZONE)) || (position & POS_FACEUP)) {
+		if(!(location & LOCATION_ONFIELD) || (position & POS_FACEUP)) {
 			pcard->enable_field_effect(true);
 			field->adjust_instant();
 		}

field.cpp
@@ -576,10 +576,14 @@ int32_t field::is_location_useable(uint32_t playerid, uint32_t location, uint32_
 		sequence += 5;
 		location = LOCATION_MZONE;
 	}
-	if(location == LOCATION_MMZONE)
+	if(location == LOCATION_MMZONE) {
 		location = LOCATION_MZONE;
-	if(location == LOCATION_STZONE)
+		sequence += 1 * is_flag(DUEL_3_COLUMNS_FIELD);
+	}
+	if(location == LOCATION_STZONE) {
 		location = LOCATION_SZONE;
+		sequence += 1 * is_flag(DUEL_3_COLUMNS_FIELD);
+	}
 	if (location == LOCATION_MZONE) {
 		if(flag & (0x1u << sequence))
 			return FALSE;

commit 959577810f89ff39a41e25f88d7be708559e764c

Fix Debug.AddCard not setting the pzone state properly
Fixes regression introduced in 99361da960d421338936c6a3c27d79e1fffc7036

libdebug.cpp
@@ -56,6 +56,7 @@ LUA_FUNCTION(AddCard) {
 		if(location == LOCATION_PZONE) {
 			location = LOCATION_SZONE;
 			sequence = field->get_pzone_index(sequence, playerid);
+			pzone = true;
 		} else if(location == LOCATION_FZONE) {
 			location = LOCATION_SZONE;
 			sequence = 5;