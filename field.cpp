d9b959a75f7681ba8b7fd8304624424d209143e1
Edoardo Lolletti <edoardo762@gmail.com>
2023-09-24 16:48:50 +0200

    Fix chain attacking by monsters with EFFECT_EXTRA_ATTACK_COUNT_MONSTER applied

--- field.cpp
+++ field.cpp
@@ -2482,7 +2482,7 @@ int32_t field::get_attack_target(card* pcard, card_vector* v, uint8_t chain_atta
 		return atype;
 	if((mcount == 0 || pcard->is_affected_by_effect(EFFECT_DIRECT_ATTACK) || core.attack_player)
 			&& !pcard->is_affected_by_effect(EFFECT_CANNOT_DIRECT_ATTACK)
-			&& !(extra_count_m && pcard->announce_count > extra_count)
+			&& !(!chain_attack && extra_count_m && pcard->announce_count > extra_count)
 			&& !(chain_attack && core.chain_attack_target))
 		pcard->direct_attackable = 1;
 	return atype;
