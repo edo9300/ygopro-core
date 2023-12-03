cb918513f115c8c2767482c601edceb63725ab95
Edoardo Lolletti <edoardo762@gmail.com>
2023-12-03 17:16:00 +0100

    Change reason player in EVENT_CONFIRM/EVENT_TOHAND_CONFIRM
    Make it be the current reason player

--- libduel.cpp
+++ libduel.cpp
@@ -884,9 +884,9 @@ LUA_STATIC_FUNCTION(ConfirmCards) {
 	bool was_during_to_hand = field.check_event(EVENT_TO_HAND);
 	card_set handgroup;
 	auto raise_confirm_event = [&](card* pcard) {
-		field.raise_single_event(pcard, 0, EVENT_CONFIRM, trigeff, reason, revealingplayer, revealingplayer, 0);
+		field.raise_single_event(pcard, 0, EVENT_CONFIRM, trigeff, reason, field.core.reason_player, revealingplayer, 0);
 		if(was_during_to_hand && (pcard->current.location == LOCATION_HAND)) {
-			field.raise_single_event(pcard, 0, EVENT_TOHAND_CONFIRM, trigeff, reason, revealingplayer, revealingplayer, 0);
+			field.raise_single_event(pcard, 0, EVENT_TOHAND_CONFIRM, trigeff, reason, field.core.reason_player, revealingplayer, 0);
 			handgroup.insert(pcard);
 		}
 	};
@@ -898,7 +898,7 @@ LUA_STATIC_FUNCTION(ConfirmCards) {
 		message->write<uint8_t>(pcard->current.location);
 		message->write<uint32_t>(pcard->current.sequence);
 		raise_confirm_event(pcard);
-		field.raise_event(pcard, EVENT_CONFIRM, trigeff, reason, revealingplayer, revealingplayer, 0);
+		field.raise_event(pcard, EVENT_CONFIRM, trigeff, reason, field.core.reason_player, revealingplayer, 0);
 	} else {
 		message->write<uint32_t>(pgroup->container.size());
 		for(auto& _pcard : pgroup->container) {
@@ -908,10 +908,10 @@ LUA_STATIC_FUNCTION(ConfirmCards) {
 			message->write<uint32_t>(_pcard->current.sequence);
 			raise_confirm_event(_pcard);
 		}
-		field.raise_event(&pgroup->container, EVENT_CONFIRM, trigeff, reason, revealingplayer, revealingplayer, 0);
+		field.raise_event(&pgroup->container, EVENT_CONFIRM, trigeff, reason, field.core.reason_player, revealingplayer, 0);
 	}
 	if(handgroup.size())
-		field.raise_event(&handgroup, EVENT_TOHAND_CONFIRM, trigeff, reason, revealingplayer, revealingplayer, 0);
+		field.raise_event(&handgroup, EVENT_TOHAND_CONFIRM, trigeff, reason, field.core.reason_player, revealingplayer, 0);
 	field.process_single_event();
 	field.process_instant_event();
 	return yield();
