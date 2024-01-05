d4919d4e5d1fe932e8a9e27b9d9df6058be574b6
Edoardo Lolletti <edoardo762@gmail.com>
2024-01-05 17:16:57 +0100

    Add android ndk 16b and android ndk 26b to ci targets
    Build the core for android with those other 2 toolchains as well, (for now android 16b has most of the c++17 features available, it's mostly used as the baseline clang compiler, version 5.0.3)

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES := card.cpp \
 				processor.cpp \
 				scriptlib.cpp
                    
-LOCAL_CFLAGS    :=  -pedantic -Wno-unused-parameter -Wextra -fvisibility=hidden -DOCGCORE_EXPORT_FUNCTIONS
+LOCAL_CFLAGS    :=  -pedantic -Wextra -fvisibility=hidden -DOCGCORE_EXPORT_FUNCTIONS
 LOCAL_CPPFLAGS := -fexceptions -fno-rtti
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua/include
 LOCAL_STATIC_LIBRARIES += liblua5.3
--- travis/build-android.sh
+++ travis/build-android.sh
@@ -2,19 +2,21 @@
 
 set -euxo pipefail
 
+NDK_VERSION=${NDK_VERSION:-"21.4.7075529"}
+
 if [[ "$OS_NAME" == "osx" ]]; then
 	ANDROID_HOME=$HOME/Library/Android/sdk
 	SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
-	echo y | $SDKMANAGER "ndk;21.4.7075529"
-	ln -sfn $ANDROID_HOME/ndk/21.4.7075529 $ANDROID_HOME/ndk-bundle
+	echo y | $SDKMANAGER "ndk;$NDK_VERSION"
+	ln -sfn $ANDROID_HOME/ndk/$NDK_VERSION $ANDROID_HOME/ndk-bundle
 	$ANDROID_HOME/ndk-bundle/ndk-build -j3
 fi
 if [[ "$OS_NAME" == "linux" ]]; then
 	ANDROID_ROOT=/usr/local/lib/android
 	ANDROID_SDK_ROOT=${ANDROID_ROOT}/sdk
 	SDKMANAGER=${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager
-	echo "y" | $SDKMANAGER "ndk;21.4.7075529"
+	echo "y" | $SDKMANAGER "ndk;$NDK_VERSION"
 	ANDROID_NDK_ROOT=${ANDROID_SDK_ROOT}/ndk-bundle
-	ln -sfn $ANDROID_SDK_ROOT/ndk/21.4.7075529 $ANDROID_NDK_ROOT
+	ln -sfn $ANDROID_SDK_ROOT/ndk/$NDK_VERSION $ANDROID_NDK_ROOT
 	$ANDROID_HOME/ndk-bundle/ndk-build -j2
 fi
