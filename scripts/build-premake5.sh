e85b0016b515ed102adb458d22e1d0e5f9d254f1
Edoardo Lolletti <edoardo762@gmail.com>
2024-05-21 11:52:47 +0200

    Don't use nproc on mac os
    Use sysctl to get the number of cores

--- scripts/build-premake5.sh
+++ scripts/build-premake5.sh
@@ -8,5 +8,12 @@ if [[ -n "${TARGET_OS:-""}" ]]; then
 	PREMAKE_FLAGS="$PREMAKE_FLAGS --os=$TARGET_OS"
 fi
 
+PROCS=""
+if [[ "$TRAVIS_OS_NAME" == "macosx" ]]; then
+    PROCS=$(sysctl -n hw.ncpu)
+else
+    PROCS=$(nproc)
+fi
+
 ./premake5 gmake2 $PREMAKE_FLAGS
-make -Cbuild ocgcoreshared -j$(nproc) config=$BUILD_CONFIG
+make -Cbuild ocgcoreshared -j$PROCS config=$BUILD_CONFIG
