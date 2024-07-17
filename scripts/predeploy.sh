3e5b74184c002fe21cdf0f2d762af03542d13aa9
Edoardo Lolletti <edoardo762@gmail.com>
2024-07-17 18:52:13 +0200

    Strip mingw binaries

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -7,6 +7,9 @@ mkdir -p $DEPLOY_DIR
 if [[ "$TARGET_OS" == "windows" ]]; then
 	shopt -s globstar
 	mv bin/**/ocgcore.dll $DEPLOY_DIR
+	if [[ "${STRIP:-0}" == "1" ]]; then
+		strip $DEPLOY_DIR/ocgcore.dll
+	fi
 elif [[ "$TARGET_OS" == "android" ]]; then
 	ARCH=("armeabi-v7a" "arm64-v8a" "x86" "x86_64" )
 	OUTPUT=("libocgcorev7.so" "libocgcorev8.so" "libocgcorex86.so" "libocgcorex64.so")
