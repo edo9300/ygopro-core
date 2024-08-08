ff952ca92f8ba1343b38a1c4f3c6921354ba0cf9
Edoardo Lolletti <edoardo762@gmail.com>
2024-08-08 17:45:43 +0200

    Build arm64 android target with 16kb page aligment
    https://developer.android.com/guide/practices/page-sizes#compile-r26-lower

--- Android.mk
+++ Android.mk
@@ -25,6 +25,9 @@ LOCAL_SRC_FILES := card.cpp \
 
 LOCAL_CFLAGS   := -pedantic -Wextra -fvisibility=hidden -DOCGCORE_EXPORT_FUNCTIONS -DNDEBUG
 LOCAL_CPPFLAGS := -fexceptions -fno-rtti
+ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+endif
 LOCAL_STATIC_LIBRARIES += liblua
 
 include $(BUILD_SHARED_LIBRARY)
