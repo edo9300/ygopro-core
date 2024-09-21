7df98f9f71211d9dfb0fe53fa3c7a1bfc207e67f
Edoardo Lolletti <edoardo762@gmail.com>
2024-09-21 18:41:07 +0200

    Pass the rights flags to lua project
    Use Speed optimizations for release, and no optimizations for debug

--- lua/premake5.lua
+++ lua/premake5.lua
@@ -3,6 +3,11 @@ project "lua"
 	pic "On"
 	files { "src/*.c", "src/*.h" }
 	removefiles { "src/lua.c", "src/luac.c", "src/ltests.h", "src/ltests.c", "src/onelua.c" }
+	
+	filter "configurations:Release"
+		optimize "Speed"	
+	filter "configurations:Debug"
+		optimize "Off"
 
 	filter { "files:**.c" }
 		compileas "C++"
