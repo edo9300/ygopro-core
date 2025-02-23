76ac85d3c0fecc12a778d3e2eaa469f2e92f76b5
Edoardo Lolletti <edoardo762@gmail.com>
2025-02-23 13:56:35 +0100

    Update copyright year on changed files and LICENSE

--- scriptlib.h
+++ scriptlib.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2024, Edoardo Lolletti (edo9300) <edoardo762@gmail.com>
+ * Copyright (c) 2016-2025, Edoardo Lolletti (edo9300) <edoardo762@gmail.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
