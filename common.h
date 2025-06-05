920b8373f3514949f0c96fafaa302a7f91b87a5a
Edoardo Lolletti <edoardo762@gmail.com>
2025-06-05 19:55:26 +0200

    Allow RACE_YOKAI to be passed to announce race

--- common.h
+++ common.h
@@ -141,8 +141,9 @@
 #define RACE_OMEGAPSYCHIC      0x20000000
 #define RACE_CELESTIALWARRIOR  0x40000000
 #define RACE_GALAXY            0x80000000
+#define RACE_YOKAI             0x4000000000000000
 #define RACE_MAX               RACE_GALAXY
-#define RACE_ALL               (~(RACE_MAX<<1))
+#define RACE_ALL               ((~(RACE_MAX<<1))|RACE_YOKAI)
 
 //Reasons
 #define REASON_DESTROY     0x1
