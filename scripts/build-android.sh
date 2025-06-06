53d74b1178e3b8057c363b24a02b547b5fbce705
Edoardo Lolletti <edoardo762@gmail.com>
2025-06-06 13:49:14 +0200

    Add ndk 27 target with and build for riscv64

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -3,10 +3,14 @@
 set -euxo pipefail
 
 PROCS=""
+EXT=""
 if [[ "$TRAVIS_OS_NAME" == "macosx" ]]; then
     PROCS=$(sysctl -n hw.ncpu)
 else
     PROCS=$(nproc)
 fi
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    EXT=.cmd
+fi
 
-$ANDROID_HOME/ndk-bundle/ndk-build -j$PROCS
+$ANDROID_HOME/ndk-bundle/ndk-build$EXT -j$PROCS
--- scripts/setup-ndk.sh
+++ scripts/setup-ndk.sh
@@ -3,7 +3,11 @@
 set -euxo pipefail
 
 NDK_VERSION=${NDK_VERSION:-"21.4.7075529"}
+EXT=""
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    EXT=.bat
+fi
 
-SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
+SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager$EXT
 echo y | $SDKMANAGER "ndk;$NDK_VERSION"
 ln -sfn $ANDROID_HOME/ndk/$NDK_VERSION $ANDROID_HOME/ndk-bundle
