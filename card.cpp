edo9300/ygopro-core

commit a629818557bdf7dcf74ecf43208ccafb4eb2a8f0

Check for the zone passed as second return value from the falue function in effect_spsummon_procedure
Removes the need to do a redoundant check in the condition calling iscanbespecialsummoned to explicitely pass the zone

card.cpp
@@ -2989,15 +2989,24 @@ void card::filter_spsummon_procedure(uint8 playerid, effect_set* peset, uint32 s
 			topos = POS_FACEUP;
 			toplayer = playerid;
 		}
-		if(peffect->is_available() && peffect->check_count_limit(playerid) && is_spsummonable(peffect)
+		if(!peffect->is_available() || !peffect->check_count_limit(playerid))
+			continue;
+		if(is_spsummonable(peffect)
 				&& ((topos & POS_FACEDOWN) || !pduel->game_field->check_unique_onfield(this, toplayer, LOCATION_MZONE))) {
 			effect* sumeffect = pduel->game_field->core.reason_effect;
 			if(!sumeffect)
 				sumeffect = peffect;
-			uint32 sumtype = peffect->get_value(this);
-			if((!summon_type || summon_type == sumtype)
-							&& pduel->game_field->is_player_can_spsummon(sumeffect, sumtype, topos, playerid, toplayer, this))
-				peset->push_back(peffect);
+			std::vector<int32> retval;
+			peffect->get_value(this, 0, &retval);
+			uint32 sumtype = retval.size() > 0 ? retval[0] : 0;
+			uint32 zone = retval.size() > 1 ? retval[1] : 0xff;
+			if(zone != 0xff && pduel->game_field->get_useable_count(this, toplayer, LOCATION_MZONE, playerid, LOCATION_REASON_TOFIELD, zone, nullptr) <= 0)
+				continue;
+			if(summon_type != 0 && summon_type != sumtype)
+				continue;
+			if(!pduel->game_field->is_player_can_spsummon(sumeffect, sumtype, topos, playerid, toplayer, this))
+				continue;
+			peset->push_back(peffect);
 		}
 	}
 }

commit fd2a557167f9e0fe19b867c325e67a3b3f9dca11

add 3rd return parameter to EFFECT_SPSUMMON_PROC's value
Partial workaround due to regression caused by previous commit https://github.com/edo9300/ygopro-core/commit/a629818557bdf7dcf74ecf43208ccafb4eb2a8f0
The 3rd value indicates whether the core should check for zone availability or not, thus reverting to the behaviour prior to the change

card.cpp
@@ -2764,7 +2764,8 @@ void card::filter_spsummon_procedure(uint8_t playerid, effect_set* peset, uint32
 			peffect->get_value(this, 0, retval);
 			uint32_t sumtype = retval.size() > 0 ? static_cast<uint32_t>(retval[0]) : 0;
 			uint32_t zone = retval.size() > 1 ? static_cast<uint32_t>(retval[1]) : 0xff;
-			if(zone != 0xff && pduel->game_field->get_useable_count(this, toplayer, LOCATION_MZONE, playerid, LOCATION_REASON_TOFIELD, zone, nullptr) <= 0)
+			bool ignore_zone_check = retval.size() > 2 ? static_cast<bool>(retval[2]) : false;
+			if(zone != 0xff && !ignore_zone_check && pduel->game_field->get_useable_count(this, toplayer, LOCATION_MZONE, playerid, LOCATION_REASON_TOFIELD, zone, nullptr) <= 0)
 				continue;
 			if(summon_type != 0 && summon_type != sumtype)
 				continue;